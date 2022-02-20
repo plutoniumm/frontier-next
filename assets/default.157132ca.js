6114b08d43e81a71c9104c8eba9836e5e67fc683
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2022-02-20 10:56:33 +0000

    Deploying to gh-pages from @ plutoniumm/frontier-next@934cb42b7e0f166ee083e4a1b690eb662539e30d 🚀

---
+++ assets/default.157132ca.js
@@ -0,0 +1,9 @@
+import{S as x,i as y,s as w,e as m,a as C,t as S,c as u,b as v,d as F,f as T,g as f,h as s,j as D,k as L,l,n as _,m as b,o as A,p as j,r as q,q as M,u as N}from"./vendor.ea8f6236.js";/* empty css                                          */function k(r){let t,e,a,c,h,o,p;return{c(){t=m("nav"),e=m("a"),a=m("img"),h=C(),o=m("div"),p=S("Frontier"),this.h()},l(n){t=u(n,"NAV",{class:!0});var i=v(t);e=u(i,"A",{href:!0,class:!0});var d=v(e);a=u(d,"IMG",{width:!0,height:!0,src:!0,alt:!0,class:!0}),h=F(d),o=u(d,"DIV",{class:!0});var E=v(o);p=T(E,"Frontier"),E.forEach(f),d.forEach(f),i.forEach(f),this.h()},h(){s(a,"width","32px"),s(a,"height","32px"),D(a.src,c="/icons/frontier.svg")||s(a,"src",c),s(a,"alt","logo"),s(a,"class","svelte-c5m511"),s(o,"class","fw4"),s(e,"href","/"),s(e,"class","\u0192 p10"),s(t,"class","\u0192 \u2206-bw w-100 p-fix svelte-c5m511")},m(n,i){L(n,t,i),l(t,e),l(e,a),l(e,h),l(e,o),l(o,p)},p:_,i:_,o:_,d(n){n&&f(t)}}}class g extends x{constructor(t){super();y(this,t,null,k,w,{})}}var I=Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:g});b("/src/layouts/default.astro",{modules:[{module:I,specifier:"component:nav",assert:{}}],hydratedComponents:[],hydrationDirectives:new Set([]),hoisted:[]});const V=A("/src/layouts/default.astro","https://astro.build","file:///home/runner/work/frontier-next/frontier-next/");j(async(r,t,e)=>{r.createAstro(V,t,e);const a=[{props:{"data-astro-id":"CL3FEDET"},children:"main.astro-CL3FEDET{margin-top:5rem;max-width:1200px;}"}];for(const c of a)r.styles.add(c);return q`${M(r,"Nav",g,{class:"astro-CL3FEDET"})}
+<main class="mx-a astro-CL3FEDET">
+    ${N(r,e.default)}
+</main>
+<footer class="w-100 p10 †c astro-CL3FEDET">
+    Fronter © 2022 | Plutonium
+</footer>
+
+`});
